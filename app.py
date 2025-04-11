2249fa8f64a1582d8ff78ed55709e9140140f52a
YeganehNobakht <m.yeganeh.n1994@gmail.com>
2025-04-11 16:41:19 +0330

    feat: update a note

--- app.py
+++ app.py
@@ -60,5 +60,43 @@ def get_note(note_id):
 
     return jsonify({'error': 'Note not found.'}), 200
 
+
+@app.route('/delete-note/<note_id>', methods=['DELETE'])
+def delete_note(note_id):
+    notes = utils.read_json("note_data.json")
+
+    # Use filter to exclude the note with the matching ID
+    updated_notes = list(filter(lambda note: note.get('id') != note_id, notes))
+
+    if len(updated_notes) == len(notes):
+        return jsonify({'error': 'Note not found.'}), 200
+
+    utils.write_json_to_file(updated_notes, "note_data.json")
+    return jsonify({'message': f'Note {note_id} deleted successfully.'})
+
+
+@app.route("/update-note", methods=['PUT'])
+def update_note():
+    data = request.get_json()
+
+    notes = utils.read_json("note_data.json")
+
+    allowed_fields = ['id', 'title', 'content', 'timestamp']
+
+    # Check for extra fields in the data
+    extra_fields = [field for field in data if field not in allowed_fields]
+    if extra_fields:
+        return jsonify({'error': f'Invalid fields: {", ".join(extra_fields)}'}), 400
+
+    # Find the note with the matching ID and update it
+    for i, note in enumerate(notes):
+        if note.get('id') == data.get("id"):
+            notes[i] = {**note, **data}  # Merge and update the note
+            utils.write_json_to_file(notes, "note_data.json")
+            return jsonify({'message': f'Note {data["id"]} updated successfully.'})
+
+    return jsonify({'error': 'Note not found.'}), 404
+
+
 if __name__ == "__main__":
     app.run(debug=True)
