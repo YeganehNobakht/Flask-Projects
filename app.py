6d3c004baa6b026807e4607f018fe89cdbaf5fa5
YeganehNobakht <m.yeganeh.n1994@gmail.com>
2025-04-11 14:58:16 +0330

    feat: add extra end points

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return "Welcome"
+
+
+@app.route('/status')
+def status():
     return "Server is running"
+
+@app.route('/about')
+def about():
+    return "This is a simple note-taking web app built with Flask"
 
 if __name__ == '__main__':
     app.run(debug=True)
